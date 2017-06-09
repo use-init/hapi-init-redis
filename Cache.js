f47dd184f0a541fb85f26c08cd1bb66bfb682f48
Hans Christian Reinl <info@drublic.de>
2017-06-09 23:47:01 +0200

    Initial commit

---
+++ Cache.js
@@ -0,0 +1,55 @@
+/**
+ * Caching interface for Hapi
+ */
+const client = require('redis');
+
+class Cache () {
+
+  /**
+   * Set up general cache
+   * @return {void}
+   */
+  constructor (config) {
+    this.config = config;
+    this.cacheClient = client.createClient(config.redisUrl);
+  }
+
+  /**
+   * Set a value to cache
+   * @param  {String}   key       Key to store value to
+   * @param  {String}   value     Value to store
+   * @param  {Number}   expiresIn Milliseconds after which the cached value gets deleted
+   * @param  {Function} callback  Callback
+   * @return {void}
+   */
+  set (key, value, expiresIn = this.config.expiresIn, callback = () => {}) {
+    value = JSON.stringify(value);
+
+    cacheClient.set(key, value, 'PX', expiresIn, (error) => {
+      if (error) {
+        throw error;
+      }
+
+      callback && callback();
+    });
+  }
+
+  /**
+   * Get a specific value from the store
+   * @param  {String}   key      Key to get
+   * @param  {Function} callback Function to fire after cached value was retrieved
+   * @return {void}
+   */
+  get (key, callback) {
+
+    cacheClient.get(key, (error, value) => {
+      try {
+        value = JSON.parse(value);
+      } catch (error) {}
+
+      callback && callback(error, value);
+    });
+  }
+}
+
+module.exports = Cache;
---
+++ README.md
@@ -0,0 +1,26 @@
+# HAPI INIT - Redis Cache plugin
+
+This plugin makes it easy to Cache any sort of data in Redis.
+
+## Usage
+
+Install via npm:
+
+    $ npm install --save hapi-init-redis
+
+### How to use
+
+    const Cache = require('hapi-init-redis');
+    const cache = new Cache({
+      redisUrl:
+    });
+
+    // Setting an object in cache
+    cache.set('foo', 'bar', 1000 * 60 * 60, () => {
+      console.log('Data set in Cache');
+    });
+
+    // Getting the object from Cache
+    cache.get('foo', (error, data) => {
+      console.log('Data found:', data);
+    });
